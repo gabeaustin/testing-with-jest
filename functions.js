6f8dc96f49eb0b2e8a379b6678526aa7aa3c8d82
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-05-07 20:11:36 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+const functions = {
+    // arrow functions
+    // add: (num1, num2) => num1 + num2,
+
+    // without arrow functions
+    add: function(num1, num2) {
+        return num1 + num2
+    }
+}
+
+module.exports = functions;
